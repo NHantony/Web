fc073f4fdd9caa5279ad4c3a89334e44fcd0e5f6
NHantony <huydoannguyen9@gmail.com>
2022-03-04 15:31:52 +0700

    tao form

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-
 namespace Todo.Controllers;
-public class TodoController : Controller {
-    
-    public IActionResult Index(){
+
+public class TodoController : Controller
+{
+
+    public IActionResult Index()
+    {
         return View();
     }
-    public IActionResult TodoForm(){
+
+    [HttpGet] //ddee truy cập dô form
+    public IActionResult TodoForm()
+    {
         return View();
     }
-    
+
+    [HttpPost]
+    public IActionResult TodoForm(Todo.Models.Todo _todo) //truyen len kieu gi thi no hứng kiểu đó --- ctroller /Models /View /teen bieens
+    {
+        return View();
+    }
 }
